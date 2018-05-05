771f3ad04740ef6c7ee7419aa3a3afda90d919ce
xyyx <xyyx@mail.ru>
2018-05-05 08:07:47 +0800

    mido: workaround Default SIM Data Reset After Reboot
    Signed-off-by: zainarbani <zaintsyariev@gmail.com>

--- prop.mk
+++ prop.mk
@@ -192,6 +192,7 @@ DEVICE_PROVISIONED=1 \
 persist.dbg.volte_avail_ovr=1 \
 persist.dbg.vt_avail_ovr=1 \
 persist.dbg.wfc_avail_ovr=1 \
+persist.radio.aosp_usr_pref_sel=true \
 persist.radio.multisim.config=dsds \
 persist.vendor.radio.apm_sim_not_pwdn=1 \
 persist.vendor.radio.atfwd.start=true \
